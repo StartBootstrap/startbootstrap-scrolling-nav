e5b7c8ea840de9f87f09dc709a2329e20713b223
Levi Robertson <robertson.l.levi@gmail.com>
2015-08-04 04:36:39 -0700

    change jQuery close mobile nav function.
    Changed $(".navbar-fixed-top").addClass("top-nav-collapse"); to $(".navbar-ex1-collapse").removeClass("in"); removed if else statement.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,10 +1,6 @@
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+    $(".navbar-ex1-collapse").removeClass("in");
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
