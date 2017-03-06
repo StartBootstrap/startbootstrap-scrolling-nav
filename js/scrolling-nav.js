c3b58e54355cb7679f9288349193c674a93608ce
David Miller <hello@davidmiller.io>
2017-03-06 16:52:40 -0500

    scroll bind fix

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,7 +9,7 @@ $(window).scroll(function() {
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
