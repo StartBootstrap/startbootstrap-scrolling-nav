749eb45227befb163377f9dc711071276a0c891f
David Miller <hello@davidmiller.io>
2017-03-22 21:29:34 -0400

    initial commit for v4-dev

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,19 +1,34 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+(function($) {
+    "use strict"; // Start of use strict
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: ($($anchor.attr('href')).offset().top - 54)
+        }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-});
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 54
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse>ul>li>a').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    });
+
+})(jQuery); // End of use strict
+
