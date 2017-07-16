8d3511228184bcc8f318a7051f15fb08304bd91c
liberationfront <bomengeduld@gmail.com>
2017-07-16 02:32:49 +0200

    ud
    scrolling nav, hovering nature black / white...
    
    deleted all grey

--- _site/js/scrolling-nav.js
+++ _site/js/scrolling-nav.js
@@ -1,14 +1,9 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
+
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+
+
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -16,4 +11,11 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+});
+
+
+// jQuery for collapsing nav when clicking
+$('.nav a').on('click', function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click(); //bootstrap 3.x by Richard
 });
