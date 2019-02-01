4bdee3bd92d01722e33518eb592853a547163c9e
David Miller <hello@davidmiller.io>
2019-01-31 20:49:09 -0500

    update dependencies, gulp 4 update, update copyright dates

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
+          scrollTop: (target.offset().top - 56)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 54
+    offset: 56
   });
 
 })(jQuery); // End of use strict
