74b89e5192c46606705ecac3d9047c3bd867f661
David Miller <hello@davidmiller.io>
2022-03-22 16:36:30 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
 */
 //
